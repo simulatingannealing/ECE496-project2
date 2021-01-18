c1abcd347a46b565b9641b61531c5a381ea3837d
danny ding <danny@capstone.(none)>
2021-01-18 17:40:37 +0000

    update the packet status table and the testbench

--- sources/packet_status_table/packet_status.v
+++ sources/packet_status_table/packet_status.v
@@ -22,7 +22,8 @@
 
 module packet_status # (
     parameter TAG_WIDTH = 6,
-    parameter CIRCULAR_BUFFER_SIZE = 50
+    parameter CIRCULAR_BUFFER_SIZE = 50,
+    parameter STATUS_TABLE_SIZE = CIRCULAR_BUFFER_SIZE * 2
 )(
     input wire clk,
     input wire rst,
@@ -33,20 +34,12 @@ module packet_status # (
     // from the circular buffer
     input wire [TAG_WIDTH-1:0] cb_reorder_tag,
     // to the circular buffer
-    output wire [1:0] cb_rd_packet_status
+    output wire [1:0] cb_rd_packet_status,
+    output reg [STATUS_TABLE_SIZE-1:0] status_table
     
 );
-    reg [1:0] status_table [CIRCULAR_BUFFER_SIZE-1:0];
+    // reg [1:0] status_table [CIRCULAR_BUFFER_SIZE-1:0];
     reg [TAG_WIDTH-1:0] status_reset_iterator;
-    
-    // initialization, simulation only
-    integer i;
-    initial begin
-        for (i = 0; i < CIRCULAR_BUFFER_SIZE; i=i+1) begin
-            status_table[i] <= 2'b00;
-        end
-        status_reset_iterator <= 0;
-    end
     
     /* status:
     00: pending
@@ -55,16 +48,21 @@ module packet_status # (
     always @(posedge clk) begin
         if (! rst) begin
             if (status_reset_iterator != cb_reorder_tag) begin
-                status_table[status_reset_iterator] <= 2'b00;
+                // status_table[status_reset_iterator] <= 2'b00;
+                status_table[{status_reset_iterator,1'b0}] <= 1'b0;
+                status_table[{status_reset_iterator,1'b1}] <= 1'b0;
             end
             if (BPF_wr_valid) begin
-                status_table[BPF_reorder_tag] <= {BPF_wr_packet_status, 1'b1};
+                // status_table[BPF_reorder_tag] <= {BPF_wr_packet_status, 1'b1};
+                status_table[{BPF_reorder_tag,1'b0}] <= 1'b1;
+                status_table[{BPF_reorder_tag,1'b1}] <= BPF_wr_packet_status;
             end
         end else begin
             // initialize the status table to "pending" status
-            for (i = 0; i < 50; i = i + 1) begin
-                status_table[i] <= 2'b00;
-            end
+            // for (i = 0; i < CIRCULAR_BUFFER_SIZE; i = i + 1) begin
+                // status_table[i] <= 2'b00;
+            // end
+            status_table <= 0;
         end
     end
     
@@ -77,6 +75,9 @@ module packet_status # (
         end
     end
     
-    assign cb_rd_packet_status = status_table[cb_reorder_tag];
+    // assign cb_rd_packet_status = status_table[cb_reorder_tag];
+    assign cb_rd_packet_status = {status_table[{cb_reorder_tag,1'b1}], status_table[{cb_reorder_tag,1'b0}]};
+    
+    
     
 endmodule
--- sources/packet_status_table/packet_status_tb.v
+++ sources/packet_status_table/packet_status_tb.v
@@ -20,28 +20,29 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 `define CLOG2(x) (\
-   (((x) <= 2) ? 1 : \
-   (((x) <= 4) ? 2 : \
-   (((x) <= 8) ? 3 : \
-   (((x) <= 16) ? 4 : \
-   (((x) <= 32) ? 5 : \
-   (((x) <= 64) ? 6 : \
-   (((x) <= 128) ? 7 : \
-   (((x) <= 256) ? 8 : \
-   (((x) <= 512) ? 9 : \
-   (((x) <= 1024) ? 10 : \
-   (((x) <= 2048) ? 11 : \
-   (((x) <= 4096) ? 12 : \
-   (((x) <= 8192) ? 13 : \
-   (((x) <= 16384) ? 14 : \
-   (((x) <= 32768) ? 15 : \
-   (((x) <= 65536) ? 16 : \
+   (((x) < 2) ? 1 : \
+   (((x) < 4) ? 2 : \
+   (((x) < 8) ? 3 : \
+   (((x) < 16) ? 4 : \
+   (((x) < 32) ? 5 : \
+   (((x) < 64) ? 6 : \
+   (((x) < 128) ? 7 : \
+   (((x) < 256) ? 8 : \
+   (((x) < 512) ? 9 : \
+   (((x) < 1024) ? 10 : \
+   (((x) < 2048) ? 11 : \
+   (((x) < 4096) ? 12 : \
+   (((x) < 8192) ? 13 : \
+   (((x) < 16384) ? 14 : \
+   (((x) < 32768) ? 15 : \
+   (((x) < 65536) ? 16 : \
    -1)))))))))))))))))
 
 module packet_status_tb;
     
     localparam TAG_WIDTH = 6;
-    localparam CIRCULAR_BUFFER_SIZE = 50;
+    localparam CIRCULAR_BUFFER_SIZE = 7;
+    localparam STATUS_TABLE_SIZE = CIRCULAR_BUFFER_SIZE * 2;
     
     /** inputs and outputs of the packet status module **/
     reg clk;
@@ -54,76 +55,88 @@ module packet_status_tb;
     reg [TAG_WIDTH-1:0] cb_reorder_tag;
     // to the circular buffer
     wire [1:0] cb_rd_packet_status;
+    wire [STATUS_TABLE_SIZE-1:0] status_table; // for simulation demo
     
     /** testbench logics **/
-    localparam BPF_WR_VALID_COUNTER_WIDTH = 1; // BPF_wr_valid every 2^BPF_WR_VALID_COUNTER_WIDTH cycles
-    localparam PACKET_NUM = 1387;
+    localparam PACKET_NUM = 61;
+    reg [`CLOG2(PACKET_NUM)-1:0] num_input_packet;
+    // simulate the signals from BPFs
+    localparam BPF_WR_VALID_COUNTER_WIDTH = 1;
     reg [BPF_WR_VALID_COUNTER_WIDTH-1:0] BPF_wr_valid_counter;
-    reg[`CLOG2(PACKET_NUM)-1:0] packet_iterator;
-    reg [TAG_WIDTH-1:0] packet_tags[PACKET_NUM-1:0];
-    reg packet_status[PACKET_NUM-1:0];
-    reg o_packet_status[PACKET_NUM-1:0];
-    reg[`CLOG2(PACKET_NUM)-1:0] num_input_packet;
-    reg[`CLOG2(PACKET_NUM)-1:0] num_output_packet;
+    // from the testbench data file
+    reg [`CLOG2(PACKET_NUM)-1:0] packet_index;
+    reg [TAG_WIDTH-1:0] packet_index_trace[0:PACKET_NUM-1];
+    reg packet_status;
+    reg [0:PACKET_NUM-1] packet_status_trace;
+    reg packet_status_in_real_order;
+    reg [0:PACKET_NUM-1] packet_status_in_real_order_trace;
+    // simulate the signals from the circular buffer
+    reg [`CLOG2(PACKET_NUM)-1:0] num_output_packet;
+    reg [`CLOG2(PACKET_NUM)-1:0] base_num_packets;
+    wire is_refreshing_circular_buffer;
     wire is_all_sent_in;
     wire is_all_sent_out;
+    // output of the packet status module
+    reg [0:PACKET_NUM-1] packet_status_from_table;
     
-    integer i, j, num_packets_left, inner_bound, index;
+    
+    integer fd, dummy;
     initial begin
         clk = 1;
         rst = 1;
         cb_reorder_tag = 0;
-        // logic to send in the packet status
-        BPF_wr_valid_counter = 0;
-        inner_bound = CIRCULAR_BUFFER_SIZE < PACKET_NUM ? CIRCULAR_BUFFER_SIZE : PACKET_NUM;
-        num_packets_left = PACKET_NUM;
-        for (i = 0; i < PACKET_NUM; i = i + CIRCULAR_BUFFER_SIZE) begin
-            for (j = 0; j < inner_bound; j = j + 1) begin
-                index = i > 0 ? (i + j) : (i + inner_bound - 1 - j);
-                packet_tags[i + j] = index % CIRCULAR_BUFFER_SIZE;
-                packet_status[index] = ((index & 3) == 0);
+        // get data from a memory file
+        fd = $fopen("packet_status_table_tb_data.mem", "r");
+        if (fd == 0) begin
+            $display("Could not open file");
+            $finish;
+        end
+        
+        while ($fgetc(fd) != "\n") begin
+            if ($feof(fd)) begin
+                $display("Error: file is in incorrect format");
+                $finish;
             end
-            num_packets_left = num_packets_left - CIRCULAR_BUFFER_SIZE; 
-            inner_bound = num_packets_left < CIRCULAR_BUFFER_SIZE ? num_packets_left : CIRCULAR_BUFFER_SIZE;
         end
         num_input_packet = 0;
-        // logic to send out the packet status
         num_output_packet = 0;
-        for (i = 0; i < PACKET_NUM; i = i + 1) begin
-            o_packet_status[i] = 1'bx;
-        end
-        packet_iterator = 0;
+        base_num_packets = 0;
+        BPF_wr_valid_counter = 0;
     end
     
     always #5 clk <= ~clk;
     
+    assign BPF_wr_valid = &BPF_wr_valid_counter;
+    assign BPF_reorder_tag = packet_index % CIRCULAR_BUFFER_SIZE;
+    assign BPF_wr_packet_status = packet_status_in_real_order;
+    assign is_refreshing_circular_buffer = (num_input_packet>base_num_packets) && 
+                                           (num_input_packet%CIRCULAR_BUFFER_SIZE==0) && 
+                                           (cb_reorder_tag < CIRCULAR_BUFFER_SIZE-1);
+    
     always @(posedge clk) begin
         rst <= 0;
+        BPF_wr_valid_counter <= BPF_wr_valid_counter + (! is_refreshing_circular_buffer);
         if (! is_all_sent_in) begin
-            BPF_wr_valid_counter <= BPF_wr_valid_counter + 1;
-            // when a BPF write arrives
-            if (BPF_wr_valid) begin
-                if (packet_iterator != PACKET_NUM - 1) begin
-                    packet_iterator <= packet_iterator + 1;
-                end else begin
-                    packet_iterator <= 0;
-                end
-                if (num_input_packet == PACKET_NUM - 1) begin
-                    $display("All packets are sent in!\n");
-                end
+            if (~BPF_wr_valid && ! is_refreshing_circular_buffer) begin
+                #0.01
+                dummy = $fscanf(fd, "%b%d%b",
+                    packet_status,
+                    packet_index,
+                    packet_status_in_real_order
+                );
+                packet_index_trace[num_input_packet] <= packet_index;
+                packet_status_trace[num_input_packet] <= packet_status;
+                packet_status_in_real_order_trace[num_input_packet] <= packet_status_in_real_order;
                 num_input_packet <= num_input_packet + 1;
             end
         end
     end
     
-    assign BPF_wr_valid = &BPF_wr_valid_counter;
-    assign BPF_reorder_tag = packet_tags[packet_iterator];
-    assign BPF_wr_packet_status = packet_status[BPF_reorder_tag];
-    assign is_all_sent_in = num_input_packet == PACKET_NUM;
     
     packet_status # (
         .TAG_WIDTH(TAG_WIDTH),
-        .CIRCULAR_BUFFER_SIZE(CIRCULAR_BUFFER_SIZE)
+        .CIRCULAR_BUFFER_SIZE(CIRCULAR_BUFFER_SIZE),
+        .STATUS_TABLE_SIZE(STATUS_TABLE_SIZE)
     ) packet_status_table (
         .clk(clk),
         .rst(rst),
@@ -134,7 +147,9 @@ module packet_status_tb;
         // from the circular buffer
         .cb_reorder_tag(cb_reorder_tag),
         // to the circular buffer
-        .cb_rd_packet_status(cb_rd_packet_status)
+        .cb_rd_packet_status(cb_rd_packet_status),
+        // for simulation demo
+        .status_table(status_table)
     );
     
     // assume the circular buffer emits a packet as soon as the status is ready
@@ -145,13 +160,14 @@ module packet_status_tb;
                 
                 // display the status of the current packet
                 $display("packet%d status in the table: %d\n", cb_reorder_tag, cb_rd_packet_status[1]);
-                o_packet_status[cb_reorder_tag] = cb_rd_packet_status[1];
+                packet_status_from_table[num_output_packet] <= cb_rd_packet_status[1];
                 
                 // point to the next packet
                 if (cb_reorder_tag < CIRCULAR_BUFFER_SIZE - 1) begin
                     cb_reorder_tag <= cb_reorder_tag + 1;
                 end else begin
                     cb_reorder_tag <= 0;
+                    base_num_packets <= base_num_packets + CIRCULAR_BUFFER_SIZE;
                 end
                 
                 // increment the total number of packets sent out
@@ -159,10 +175,19 @@ module packet_status_tb;
             end
         end else begin
             $display("All packets are sent out!\n");
-            // $stop();
+            if (packet_status_trace == packet_status_from_table) begin
+                $display("PASS!\n");
+            end else begin
+                $display("FAIL!\n");
+            end
         end
     end
     
     assign is_all_sent_out = num_output_packet == PACKET_NUM;
+    assign is_all_sent_in = num_input_packet == PACKET_NUM;
+    
+    // for simulation demo
+    wire [1:0] updated_packet_status;
+    assign updated_packet_status = {BPF_wr_packet_status, 1'b1};
     
 endmodule
